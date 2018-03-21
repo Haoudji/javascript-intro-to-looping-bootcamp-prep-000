b0b19e0aca1d57e8acd31aa9648f5bfafa144419
Issa Boubacar <bacaris78@gmail.com>
2018-03-21 20:49:39 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,11 @@
+function forLoop(array){
+  array.length = 25;
+  for(var i = 0; i <= array.length; i++){
+    if(i===1){
+      array[i] = "I am 1 strange loop."
+    }else{
+      array[i] = "I am ${i} strange loops."
+  }
+  }
+  return array[i]
+}
